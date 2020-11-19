71dd07cc6c9b3a721677a6509aabe69b5750f2e1
Jesus <jesusd.alvarado@gmail.com>
2020-11-19 22:02:09 +0100

    Creating resources: iam instance profile and iam role

--- main.tf
+++ main.tf
@@ -17,3 +17,29 @@ provider "aws" {
   profile = "default"
   region = var.AWS_REGION
 }
+
+
+resource "aws_iam_instance_profile" "eks_test_profile" {
+  name = "eks_test_profile"
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_role" "role" {
+  name = "eks_test_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow"
+        }
+    ]
+}
+EOF
+}
