2e7eb3a39974fa38ee3d0ec70bad8a2c258c835a
Jesus <jesusd.alvarado@gmail.com>
2020-12-01 10:46:08 +0100

    Creating VPC and subnets (required for aws_eks_cluster resource)

--- main.tf
+++ main.tf
@@ -95,3 +95,29 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "example1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Example1"
+  }
+}
+
+resource "aws_subnet" "example2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Example2"
+  }
+}
