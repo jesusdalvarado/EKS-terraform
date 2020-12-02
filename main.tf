8d03150a24c4967766514af94c8897c68b7440cc
Jesus <jesusd.alvarado@gmail.com>
2020-12-02 14:15:25 +0100

    Configuring subnets to be in different AZ (availability zones) to fulfil EKS requirements

--- main.tf
+++ main.tf
@@ -110,8 +110,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "example1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "Example1"
@@ -119,8 +120,9 @@ resource "aws_subnet" "example1" {
 }
 
 resource "aws_subnet" "example2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
 
   tags = {
     Name = "Example2"
