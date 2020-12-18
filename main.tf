1907504234c661c5b03af04327ce0d81d6729607
Jesus <jesusd.alvarado@gmail.com>
2020-12-18 17:21:23 +0100

    Removing unused security group resource

--- main.tf
+++ main.tf
@@ -69,54 +69,6 @@ resource "aws_iam_role_policy_attachment" "example-amazon-eks-vpc-resource-contr
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
-# resource "aws_security_group" "allow_tls" {
-#   name        = "allow_tls"
-#   description = "Allow TLS inbound traffic"
-
-#   ingress {
-#     description = "TLS from VPC"
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     description = "HTTP"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     description = "Allow SSH"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     description = "Allow Redis"
-#     from_port   = 6379
-#     to_port     = 6379
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "allow_tls"
-#   }
-# }
-
 resource "aws_vpc" "main" {
   cidr_block            = "10.0.0.0/16"
   enable_dns_support    = true
