f6fd68b0582b87c787011ab4b99032fd121291cc
Jesus <jesusd.alvarado@gmail.com>
2020-12-21 20:33:12 +0100

    Editing app to connect to redis server

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import time
+import redis
 from flask import Flask
+import os
+
+REDIS_URL = os.environ['REDIS_URL']
 
 app = Flask(__name__)
+cache = redis.Redis(host=REDIS_URL, port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
